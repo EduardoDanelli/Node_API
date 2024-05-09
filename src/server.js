c506d70e914c392ab7f3c80e04bee414299e8e00
Eduardo <eduardodanelli@gmail.com>
2024-05-08 22:04:26 -0300

    envio do projeto concluído

--- src/server.js
+++ src/server.js
@@ -1,18 +1,33 @@
 import http from 'http'
 
-const server = http.createServer((req, res) => {
+import { json } from './middlewares/json.js'
+import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
+
+const server = http.createServer(async (req, res) => {
     
     const { method, url} = req
 
-    if (method === 'GET' && url === '/users'){
-        return res.end('Listagem de usuários')
-    }
+    await json(req, res)
 
-    if (method === 'POST' && url === '/users') {
-        return res.end('Criação de usuários')
+    const route = routes.find(route => {
+        return route.method === method && route.path.test(url)
+    })
+
+    if(route){
+
+        const routeParams = req.url.match(route.path)
+
+        const { query, ...params } = routeParams.groups
+
+        req.params = params
+
+        req.query = query ? extractQueryParams(query) : {}
+
+        return route.handler(req, res)
     }
     
-    return res.end('Sem rota')
+    return res.writeHead(404).end('Not Found')
 })
 
 server.listen(3333)
